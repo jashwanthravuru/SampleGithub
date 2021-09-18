abded68bdd27e53fdf6803154faea08b3971fa4c
jashwanthravuru <80572166+jashwanthravuru@users.noreply.github.com>
2021-09-18 18:14:46 +0530

    First Commit.....

---
+++ src/main/java/SampleGithub/SampleGithub/App.java
@@ -0,0 +1,13 @@
+package SampleGithub.SampleGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/BasicJava/StringProgram.java
@@ -0,0 +1,19 @@
+package BasicJava;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Sai\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+
+	}
+
+}
